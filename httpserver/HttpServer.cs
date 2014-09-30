3e914ce14d8fb073da7754f1b7745631e8b75056
Mihai Constantin <mihai.cfs@gmail.com>
2014-09-30 12:25:01 -0700

    some more stuff, not quite doing much yet

--- httpserver/HttpServer.cs
+++ httpserver/HttpServer.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace httpserver
@@ -19,14 +21,49 @@ namespace httpserver
 
         public void serverStart()
         {
+
             TcpListener serverSocket = new TcpListener(DefaultPort);
             serverSocket.Start();
-            
 
-        }
+            while (true)
+            {
+                TcpClient s = serverSocket.AcceptTcpClient();
+                Console.WriteLine("Server Online... +*+*+*+");
+
+                Socket connectionSocket = serverSocket.AcceptSocket();
+                Console.WriteLine("Server activated");
+
+                Stream ns = new NetworkStream(connectionSocket);
+
+                StreamReader sr = new StreamReader(ns);
+                StreamWriter sw = new StreamWriter(ns);
+                sw.AutoFlush = true; // enable automatic flushing
+
+                string message = sr.ReadLine();
+                string answer = "";
+                while (message != null && message != "")
+                {
+
+                    Console.WriteLine("Client: " + message);
+                    answer = message.ToUpper();
+                    sw.WriteLine(answer);
+                    message = sr.ReadLine();
+
+                    Thread myThread = new Thread(new ThreadStart(serverStart));
+                    myThread.Start();
+
+
+                }
+
+                connectionSocket.Close();
+
+
+
+            }
 
 
 
         }
     }
 
+}
